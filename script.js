3f614b1452e02751d628ee8f56eeeaa2e438da08
sravary <sravary@hotmail.com>
2019-10-24 08:56:16 -0400

    add js functionality

---
+++ script.js
@@ -0,0 +1,62 @@
+const todoList = {
+  todos: [],
+  displayTodos: function () {
+    if (this.todos.length === 0) {
+      console.log('Your TODO list is empty!');
+    } else {
+      console.log('My TODOS:');
+      for (let i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    let totalTodos = this.todos.length;
+    let completedTodos = 0;
+
+    //Get number of completed todos.
+    for (let i = 0; i < totalTodos; i++) {
+      if(this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    //Case 1: If everything is true, make everything false
+    if (completedTodos === totalTodos) {
+      //Make everything false
+      for (let i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+      //Case 2: Otherwise, make everything true
+    } else {
+      for (let i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
+  }
+};
